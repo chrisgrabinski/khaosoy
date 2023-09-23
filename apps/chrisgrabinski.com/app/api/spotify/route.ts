9d483bb11712de41ab2ee501a4cb431b45564387
Chris Grabiński <hello@chrisgrabinski.com>
2023-09-23 10:30:09 +0100

    remove api route after debugging issue

--- apps/chrisgrabinski.com/app/api/spotify/route.ts
+++
@@ -1,8 +0,0 @@
-import { getCurrentlyPlaying } from "@/lib/spotify";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = await getCurrentlyPlaying().then((res) => res.json());
-
-  return NextResponse.json({ data });
-}
